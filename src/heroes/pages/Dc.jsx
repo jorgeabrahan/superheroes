a0a23e3013795592c9c7053a849e4a06d9a4a338
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-14 22:27:03 -0600

    Create page to display dc heroes

---
+++ src/heroes/pages/Dc.jsx
@@ -0,0 +1,12 @@
+import { HeroList } from '../components'
+
+export const Dc = () => {
+  return (
+    <>
+      <h1>Dc Comics</h1>
+      <hr />
+
+      <HeroList publisher="DC Comics" />
+    </>
+  )
+}
