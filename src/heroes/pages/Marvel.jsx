e05bafe0d4b03eda40f15a5f0729cf58765abe99
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-14 22:27:17 -0600

    Create page to display marvel heroes

---
+++ src/heroes/pages/Marvel.jsx
@@ -0,0 +1,12 @@
+import { HeroList } from '../components'
+
+export const Marvel = () => {
+  return (
+    <>
+      <h1>Marvel Comics</h1>
+      <hr />
+
+      <HeroList publisher="Marvel Comics" />
+    </>
+  )
+}
