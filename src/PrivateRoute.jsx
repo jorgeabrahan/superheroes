295ee0c7c372bda7161384326f12c2cd974ea295
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-14 20:39:40 -0600

    Create private route wrapper component

---
+++ src/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import { useContext } from 'react';
+import { AuthContext } from './auth';
+import { Navigate, useLocation } from 'react-router-dom';
+
+export const PrivateRoute = ({ children }) => {
+  const { authState } = useContext(AuthContext)
+  const { pathname, search } = useLocation()
+  const lastPath = pathname + search
+  localStorage.setItem('lastPath', lastPath)
+  return authState?.logged ? children : <Navigate to='/login' />
+}
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
